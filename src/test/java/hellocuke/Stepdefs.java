9efa129e03a81efdaa6183f2179be4de9d4cbe55
Andrea C. Crawford <acm@Us.ibm.com>
2019-09-18 11:47:29 -0400

    added in recommended Cucumber snippets

--- src/test/java/hellocuke/Stepdefs.java
+++ src/test/java/hellocuke/Stepdefs.java
@@ -7,5 +7,46 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+@Given("I am a Standard Frequent Flyer member")
+public void i_am_a_Standard_Frequent_Flyer_member() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@When("I fly on a flight that is worth {int} base points")
+public void i_fly_on_a_flight_that_is_worth_base_points(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Then("I should earn a status bonus of {int}")
+public void i_should_earn_a_status_bonus_of(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Then("I should have guaranteed minimum points of {int}")
+public void i_should_have_guaranteed_minimum_points_of(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Then("I should earn {int} points in all")
+public void i_should_earn_points_in_all(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Given("I am a Silver Frequent Flyer member")
+public void i_am_a_Silver_Frequent_Flyer_member() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Given("I am a Gold Frequent Flyer member")
+public void i_am_a_Gold_Frequent_Flyer_member() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
 
 }
